4fff8c31ca49266119ece6b05519f57dc9eec69e
Robert Boutillier <36680316+rbout@users.noreply.github.com>
2019-09-19 15:37:36 -0400

    Changed function name

--- main.cpp
+++ main.cpp
@@ -8,20 +8,20 @@ int main() {
     cout << "Hello, User!" << endl;
     int list[6] = {5,2,10,11,3,13};
     int small = list[0];
-    int smallest = sort(list, small, 0, 6);
+    int smallest = findsmall(list, small, 0, 6);
     cout << "Smallest number: " << smallest << endl;
     return 0;
 }
 
-int sort(int list[], int small, int iter, int length){
+int findsmall(int list[], int small, int iter, int length){
     if(length == 0)
         return NULL;
     else if(iter == length)
         return small;
     else if(list[iter] < small){
         small = list[iter];
-        return sort(list, small, iter+1, 6);
+        return findsmall(list, small, iter+1, 6);
     }
     else
-        return sort(list, small, iter+1, 6);
+        return findsmall(list, small, iter+1, 6);
 }
